7bdfc6392a168e7f0e4f061127574f059bff02c9
Evgeniy Tsoy <EvTsoy@MacBook.local>
2021-03-28 17:16:39 +0500

    login feature

--- october_cms/resources/views/admin/body/sidebar.blade.php
+++ october_cms/resources/views/admin/body/sidebar.blade.php
@@ -8,7 +8,7 @@
 				  <!-- logo for regular state and mobile devices -->
 					 <div class="d-flex align-items-center justify-content-center">
 						  <img src="{{ asset('backend/images/logo-dark.png') }}" alt="">
-						  <h3><b>Easy</b> Admin</h3>
+						  <h3><b>Super Easy</b> Admin</h3>
 					 </div>
 				</a>
 			</div>
